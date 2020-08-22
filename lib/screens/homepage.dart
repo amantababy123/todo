38844514e3c12bc5c8c207cf2edde1766fb446e2
Akshaye <akshayejh@gmail.com>
2020-08-22 21:44:46 +0530

    Project Finished

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:what_todo/database_helper.dart';
+import 'package:what_todo/screens/taskpage.dart';
 import 'package:what_todo/widgets.dart';
 
 class Homepage extends StatefulWidget {
@@ -7,16 +9,15 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  DatabaseHelper _dbHelper = DatabaseHelper();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Container(
           width: double.infinity,
-          padding: EdgeInsets.symmetric(
-            horizontal: 24.0,
-            vertical: 32.0,
-          ),
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
           color: Color(0xFFF6F6F6),
           child: Stack(
             children: [
@@ -25,34 +26,80 @@ class _HomepageState extends State<Homepage> {
                 children: [
                   Container(
                     margin: EdgeInsets.only(
+                      top: 32.0,
                       bottom: 32.0,
                     ),
                     child: Image(
-                      image: AssetImage(
-                        'assets/images/logo.png'
-                      ),
+                      image: AssetImage('assets/images/logo.png'),
                     ),
                   ),
-                  TaskCardWidget(
-                    title: "Get Started!",
-                    desc: "Hello User! Welcome to WHAT_TODO app, this is a default task that you can edit or delete to start using the app.",
-                  ),
-                  TaskCardWidget(),
+                  Expanded(
+                    child: FutureBuilder(
+                      initialData: [],
+                      future: _dbHelper.getTasks(),
+                      builder: (context, snapshot) {
+                        return ScrollConfiguration(
+                          behavior: NoGlowBehaviour(),
+                          child: ListView.builder(
+                            itemCount: snapshot.data.length,
+                            itemBuilder: (context, index) {
+                              return GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => Taskpage(
+                                        task: snapshot.data[index],
+                                      ),
+                                    ),
+                                  ).then(
+                                    (value) {
+                                      setState(() {});
+                                    },
+                                  );
+                                },
+                                child: TaskCardWidget(
+                                  title: snapshot.data[index].title,
+                                  desc: snapshot.data[index].description,
+                                ),
+                              );
+                            },
+                          ),
+                        );
+                      },
+                    ),
+                  )
                 ],
               ),
               Positioned(
-                bottom: 0.0,
+                bottom: 24.0,
                 right: 0.0,
-                child: Container(
-                  width: 60.0,
-                  height: 60.0,
-                  decoration: BoxDecoration(
-                    color: Color(0xFF7349FE),
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  child: Image(
-                    image: AssetImage(
-                      "assets/images/add_icon.png",
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => Taskpage(
+                                task: null,
+                              )),
+                    ).then((value) {
+                      setState(() {});
+                    });
+                  },
+                  child: Container(
+                    width: 60.0,
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [Color(0xFF7349FE), Color(0xFF643FDB)],
+                          begin: Alignment(0.0, -1.0),
+                          end: Alignment(0.0, 1.0)),
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    child: Image(
+                      image: AssetImage(
+                        "assets/images/add_icon.png",
+                      ),
                     ),
                   ),
                 ),
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class TaskCardWidget extends StatelessWidget {
   final String title;
   final String desc;
+
   TaskCardWidget({this.title, this.desc});
 
   @override
@@ -47,5 +48,62 @@ class TaskCardWidget extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class TodoWidget extends StatelessWidget {
+  final String text;
+  final bool isDone;
+
+  TodoWidget({this.text, @required this.isDone});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 24.0,
+        vertical: 8.0,
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: 20.0,
+            height: 20.0,
+            margin: EdgeInsets.only(
+              right: 12.0,
+            ),
+            decoration: BoxDecoration(
+              color: isDone ? Color(0xFF7349FE) : Colors.transparent,
+              borderRadius: BorderRadius.circular(6.0),
+              border: isDone ? null : Border.all(
+                color: Color(0xFF86829D),
+                width: 1.5
+              )
+            ),
+            child: Image(
+              image: AssetImage('assets/images/check_icon.png'),
+            ),
+          ),
+          Flexible(
+            child: Text(
+              text ?? "(Unnamed Todo)",
+              style: TextStyle(
+                color: isDone ? Color(0xFF211551) : Color(0xFF86829D),
+                fontSize: 16.0,
+                fontWeight: isDone ? FontWeight.bold : FontWeight.w500,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NoGlowBehaviour extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+    BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
   }
 }
